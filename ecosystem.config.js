0dbe2a34e4b6e515f1229b45b8b4a1408543a49c
Alexis Tyler <xo@wvvw.me>
2021-03-23 17:55:43 +1030

    chore: add deployment file

---
+++ ecosystem.config.js
@@ -0,0 +1,34 @@
+const commonDeploy = {
+	user: 'xo',
+	host: '165.227.220.113',
+	key: '~/.ssh/deploy.key',
+	ref: 'origin/main',
+	repo: 'https://github.com/lunabot/landing-page',
+	path: '/home/xo/code/lunabot/landing-page/dev',
+	'pre-deploy': 'git reset --hard',
+	'post-deploy': 'pm2 startOrGracefulReload ecosystem.config.js --env dev'
+};
+
+module.exports = {
+	apps: [
+		{
+			name: 'lunabot.org',
+			script: 'serve',
+			time: true,
+			// eslint-disable-next-line
+			append_env_to_name: true,
+			instances: 1,
+			autorestart: true,
+			// eslint-disable-next-line
+			max_restarts: 50,
+			watch: true
+		}
+	],
+	deploy: {
+		production: {
+			...commonDeploy,
+			path: '/home/xo/code/lunabot/landing-page/production',
+			'post-deploy': 'pm2 startOrGracefulReload ecosystem.config.js --env production'
+		}
+	}
+};
