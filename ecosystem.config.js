e570f8a5d5d59699a8e772f887ece7a2aeab0a43
Alexis Tyler <xo@wvvw.me>
2021-03-23 17:57:49 +1030

    fix: use spa mode to serve index.htm

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -21,7 +21,13 @@ module.exports = {
 			autorestart: true,
 			// eslint-disable-next-line
 			max_restarts: 50,
-			watch: true
+			watch: true,
+			env: {
+				PM2_SERVE_PATH: '.',
+				PM2_SERVE_PORT: 8080,
+				PM2_SERVE_SPA: 'true',
+				PM2_SERVE_HOMEPAGE: './index.html'
+			}
 		}
 	],
 	deploy: {
